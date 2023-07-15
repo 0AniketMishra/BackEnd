37d64b5330657fa02dee87cd5f89a0897ad5f6a1
ConnectionsLLC <0aniketmishra@gmail.com>
2023-07-15 11:33:44 +0000

    Changing parameter name.

--- Models/Post.js
+++ Models/Post.js
@@ -37,7 +37,7 @@ const postSchema = new mongoose.Schema({
         type: String,
         default: ''
     },
-    replyingId: {
+    replyingEmail: {
         type: String,
         default: ''
     },
--- routes/uploadMediaRoutes.js
+++ routes/uploadMediaRoutes.js
@@ -28,7 +28,7 @@ const nodemailer = require('bcrypt')
 //     })
 // })
 router.post("/addpost", async (req, res) => {
-    const {  posttext, email,image1, image2, image3,image4,replyingTo,replyingOn,replyingId } = req.body;
+    const {  posttext, email,image1, image2, image3,image4,replyingTo,replyingOn,replyingEmail } = req.body;
   
     if (!posttext || !email) {
         return res.status(422).json({ error: 'please add all the fields' })
@@ -43,7 +43,7 @@ router.post("/addpost", async (req, res) => {
             image4,
             replyingTo,
             replyingOn,
-            replyingId,
+            replyingEmail,
         })
         try {
             await post.save();
